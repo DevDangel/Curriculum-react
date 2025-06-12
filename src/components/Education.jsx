d84a0eba233200aa318bfba4f9390b467595bc0e
DevDangel <angeldavidh18@gmail.com>
2025-06-12 03:37:59 -0500

    curriculum react

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,11 @@
+import '../assets/css/Education.css';
 const Education = () => {
     return (
         <div className="education">
             <section>
-                <h2>EDUCACIÓN</h2><br />
-                <p>2010 - 2016<br />BACHILLER ACADÉMICO</p><br />
-                <p>2018 - 2020<br />TÉCNICO CRIMINALISTICA</p>
+                <h2 id='educacion'>EDUCACIÓN</h2><br />
+                <p id='p-e'>2010 - 2016<br />BACHILLER ACADÉMICO</p><br />
+                <p id='p-e'>2018 - 2020<br />TÉCNICO CRIMINALISTICA</p>
             </section>
         </div>
     );
