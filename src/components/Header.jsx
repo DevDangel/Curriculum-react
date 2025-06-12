64c429375dc570b5d798dfb9a482736db65e6f49
Angel H <angeldavidh18@gmail.com>
2025-06-12 15:01:30 -0500

    curriculim + react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () =>{
         <div className="header">
             <header>
                 <h1 id='title'>ÁNGEL DAVID HERRERA ACEVEDO</h1>
-                <h1 id='parraf'>Estudiante de Ánalisis y desarrollo de software(SENA)</h1>
+                <h1 id='parraf'>Estudiante de Ánalisis y Desarrollo de Software(SENA)</h1>
                 <div className="marron"> </div>
             </header>
         </div>
