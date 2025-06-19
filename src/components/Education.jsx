053f185f8dfd9611fe6eab26196839057f932140
Angel H <angeldavidh18@gmail.com>
2025-06-19 13:14:09 -0500

    curriculum + react

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,9 +3,10 @@ const Education = () => {
     return (
         <div className="education">
             <section>
-                <h2 id='educacion'>EDUCACIÓN</h2><br />
-                <p id='p-e'>2010 - 2016<br />BACHILLER ACADÉMICO</p><br />
-                <p id='p-e'>2018 - 2020<br />TÉCNICO CRIMINALISTICA</p>
+                <h2 id='educacion'>EDUCACIÓN</h2>
+                <p id='p-e'><span id="span">2024 - Actualidad</span><br />Tecnólogo en Análisis y desarrollo de software</p>
+                <p id='p-e'><span id="span">2018 - 2020</span><br />Técnico Criminalistica</p>
+                <p id='p-e'><span id="span">2010 - 2016</span><br />Bachiller Acádemico</p>
             </section>
         </div>
     );
