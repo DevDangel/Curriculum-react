a410b0a62518f2c53e4ce9e07445fb0850ac367f
DevDangel <angeldavidh18@gmail.com>
2025-06-27 16:35:44 -0500

    curriculum * react

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,9 @@ const Contact = () => {
         <div className="contact">
             <section>
                 <h2 id='contacto'>CONTACTOS</h2><br />
-                <p id='p'><i className="fa-solid fa-phone"></i> <a href="https://wa.link/7p2zmo" target='_blank'>301 6755558</a></p>
+                <p id='p'><i className="fa-solid fa-phone"></i> <a href="https://wa.link/7p2zmo" target='_blank' title='WhatsApp'>301 6755558</a></p>
                 <p id='p'><i className="fa-solid fa-envelope"></i> angeldavidh@gmail.com</p>
-                <p id='p'><i className="fa-brands fa-github"></i><a href="https://github.com/DevDangel" target='_blank'> github.com/DevDangel</a></p>
+                <p id='p'><i className="fa-brands fa-github"></i><a href="https://github.com/DevDangel" target='_blank' title='github'> github.com/DevDangel</a></p>
                 <p id='p'><i className="fa-solid fa-location-dot"></i> Ibagué,Tolima - Colombia</p>
             </section>
         </div>
