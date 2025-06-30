a69f44def502c433a95ac89855948955dbf099b3
DevDangel <angeldavidh18@gmail.com>
2025-06-29 22:57:03 -0500

    curriculum * react

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,7 +2,7 @@ import '../assets/css/Education.css';
 const Education = () => {
     return (
         <div className="education">
-            <section>
+            <section id='education'>
                 <h2 id='educacion'>EDUCACIÓN</h2>
                 <p id='p-e'><span id="span">2024 - Actualidad</span><br />Tecnólogo en Análisis y Desarrollo de Software</p>
                 <p id='p-e'><span id="span">2018 - 2020</span><br />Técnico Criminalistica</p>
